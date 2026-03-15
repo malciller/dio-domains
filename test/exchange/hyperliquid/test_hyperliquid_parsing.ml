94ee28957851d80fbe6e1d5be1074be3c273187e
malciller <46381560+malciller@users.noreply.github.com>
2026-03-15 14:08:27 -0400

    remove old test

--- test/exchange/hyperliquid/test_hyperliquid_parsing.ml
+++ test/exchange/hyperliquid/test_hyperliquid_parsing.ml
@@ -102,33 +102,6 @@ let test_parse_action_success_empty_statuses () =
       Alcotest.(check bool) "mock order id generated" true (String.starts_with ~prefix:"hl-" oid)
   | Error e -> Alcotest.fail ("Should have succeeded with mock ID but failed with: " ^ e)
 
-let test_pack_batch_modify_action () =
-  let order : Hyperliquid.Hyperliquid_types.order_wire = {
-    a = 1;
-    b = true;
-    p = "50000.0";
-    s = "0.1";
-    r = false;
-    t = Limit { tif = Gtc };
-    c = Some "cloid-123";
-  } in
-  let msgpck = Hyperliquid.Hyperliquid_types.pack_batch_modify_action 
-    ~modifies:[(12345L, order)] 
-    ~grouping:"na" in
-  match msgpck with
-  | Msgpck.Map pairs ->
-      let get_val k = List.assoc (Msgpck.String k) pairs in
-      Alcotest.(check string) "type" "batchModify" (match get_val "type" with String s -> s | _ -> "");
-      Alcotest.(check string) "grouping" "na" (match get_val "grouping" with String s -> s | _ -> "");
-      let modifies_list = match get_val "modifies" with List l -> l | _ -> [] in
-      Alcotest.(check int) "modifies length" 1 (List.length modifies_list);
-      let mod_map = match List.hd modifies_list with Map m -> m | _ -> [] in
-      let get_mod_val k = List.assoc (Msgpck.String k) mod_map in
-      Alcotest.(check int64) "oid" 12345L (match get_mod_val "oid" with Int64 i -> i | _ -> 0L);
-      let order_map = match get_mod_val "order" with Map m -> m | _ -> [] in
-      let get_order_val k = List.assoc (Msgpck.String k) order_map in
-      Alcotest.(check string) "order p" "50000.0" (match get_order_val "p" with String s -> s | _ -> "")
-  | _ -> Alcotest.fail "Should have returned a Map"
 
 let test_parse_modify_response () =
   let json_str = {|
@@ -209,8 +182,5 @@ let () =
       Alcotest.test_case "action success empty statuses" `Quick test_parse_action_success_empty_statuses;
       Alcotest.test_case "modify response parsing" `Quick test_parse_modify_response;
       Alcotest.test_case "batchModify response parsing" `Quick test_parse_batch_modify_response;
-    ];
-    "action packing", [
-      Alcotest.test_case "pack batch modify action" `Quick test_pack_batch_modify_action;
     ];
   ]
